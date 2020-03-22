d05903532a91790060e2e29075258072a7daf5e8
Jet Balagtas <jetbalagtas@gmail.com>
2020-03-22 15:11:59 -0400

    initial commit, create todo design

---
+++ app/src/main/java/com/jetbalagtas/todolist/CreateToDo.kt
@@ -0,0 +1,12 @@
+package com.jetbalagtas.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateToDo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_to_do)
+    }
+}
